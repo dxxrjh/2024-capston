1fd0d1998cbdfecc6c7cb8e4263336a106bda29d
Physics-Computer <ms_0710_@naver.com>
2025-02-23 18:06:03 +0900

    feat: 개인 정보 수정 페이지  구현, 메인페이지- 버튼 생성

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Main() {
   const router = useRouter()
+  const [isCategoryPopupOpen, setIsCategoryPopupOpen] = useState(false);
+  const [isDatePopupOpen, setIsDatePopupOpen] = useState(false);
+  const [isSortPopupOpen, setIsSortPopupOpen] = useState(false);
 
   const searchButtonClick = () => {
     router.push("/search_result")
+  }
+
+  const toggleCategoryPopup = () => {
+    setIsCategoryPopupOpen(!isCategoryPopupOpen);
   }
 
 
@@ -31,26 +39,100 @@ export default function Main() {
       <div className="font-bold mt-[20px] flex flex-row justify-between items-center px-[290px]">
 
         <div>
-          <button className="shadow-custom w-[108px] border border-[#EBEBEB] rounded-[17%] text-[#9A9A9A] px-4 py-2">
+          <button
+            onClick={toggleCategoryPopup}
+            className="shadow-custom w-[108px] border border-[#EBEBEB] rounded-[10px] text-[#9A9A9A] px-4 py-2">
             카테고리
           </button>
+
+          {isCategoryPopupOpen && (
+            <div className="text-[15px] mt-2 w-[108px] h-[95px] bg-white border border-[#EBEBEB] rounded-[10px] shadow-custom">
+              <div className="flex flex-row">
+
+                <div className="px-4 py-2 text-left">
+                  영화
+                </div>
+
+                <button>
+                  <Image src="/images/icon_delete.svg" alt="delete button" width={15} height={15} ></Image>
+                </button>
+
+              </div>
+
+              <div className="flex flex-row border-t">
+
+                <div className="px-4 py-2 text-left">
+                  공연
+                </div>
+                <button>
+                  <Image src="/images/icon_delete.svg" alt="delete button" width={15} height={15} ></Image>
+                </button>
+              </div>
+
+              <div className="flex flex-row border-t">
+
+                <div className="px-4 py-2 text-left">
+                  전시
+                </div>
+                <button>
+                  <Image src="/images/icon_delete.svg" alt="delete button" width={15} height={15} ></Image>
+                </button>
+              </div>
+
+            </div>
+          )}
         </div>
 
 
-        <div>
-          <button className="shadow-custom w-[80px] border border-[#EBEBEB] rounded-[17%] text-[#9A9A9A] px-4 py-2 mr-[30px]">
-            일정
-          </button>
+        <div className="flex flex-row">
+          <div>
+            <button className="shadow-custom w-[80px] border border-[#EBEBEB] rounded-[17%] text-[#9A9A9A] px-4 py-2 mr-[30px]">
+              일정
+            </button>
 
-          <button className="shadow-custom w-[80px] border border-[#EBEBEB] rounded-[17%] text-[#9A9A9A] px-4 py-2">
-            정렬
-          </button>
+            {isCategoryPopupOpen && (
+              <div className=" mt-2 w-[108px] bg-white border border-[#EBEBEB] rounded-[10px] shadow-custom">
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  영화
+                </button>
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  공연
+                </button>
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  전시
+                </button>
+              </div>
+            )}
+          </div>
+
+          <div>
+
+            <button className="shadow-custom w-[80px] border border-[#EBEBEB] rounded-[17%] text-[#9A9A9A] px-4 py-2">
+              정렬
+            </button>
+
+            {isCategoryPopupOpen && (
+              <div className=" left-0 mt-2 w-[108px] bg-white border border-gray-300 rounded-lg shadow-custom">
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  옵션 1
+                </button>
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  옵션 2
+                </button>
+                <button className="block w-full px-4 py-2 text-left hover:bg-gray-100">
+                  옵션 3
+                </button>
+              </div>
+            )}
+
+          </div>
+
         </div>
 
       </div>
 
 
-    </div>
+    </div >
 
   );
 }
--- src/pages/mypage/modify.tsx
+++ src/pages/mypage/modify.tsx
@@ -5,15 +5,71 @@ export default function Modify() {
 
 
   return (
-    <div>
+    <div className="flex justify-center">
 
-    <MypageSidebar />
+      <MypageSidebar />
+      <div className="mt-[100px] flex flex-col justify-center items-center">
+        <div className="grid grid-cols-[115px_auto] grid-rows-4 w-[590px] h-[272px] border rounded-[17px] shadow-custom">
 
-    
+          {/* 2x3 부분 */}
+          <div className="text-[#9A9A9A] border p-4 text-center">닉네임</div>
+
+          <div className="border p-4">
+            <textarea className="resize-none border h-[40px] text-[14px] rounded-8px"
+              placeholder="value">
+            </textarea>
+          </div>
+
+          <div className="text-[#9A9A9A] border p-4 text-center">나이</div>
+
+          <div className="border p-4 text-center">
+            <select className="border rounded px-2 py-1 w-full max-h-32 overflow-auto">
+              {Array.from({ length: 41 }, (_, i) => 15 + i).map((age) => (
+                <option key={age} value={age}>{age}</option>
+              ))}
+            </select>
+          </div>
+
+
+          <div className="text-[#9A9A9A] border p-4 text-center">성별</div>
+
+
+          <div className="border p-4 text-center flex flex-row gap-4">
+            <label className="flex items-center space-x-2">
+              <input type="radio" name="gender" value="male" className="w-5 h-5 text-blue-600 border-gray-300 focus:ring-blue-500" />
+              <span>남성</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input type="radio" name="gender" value="female" className="w-5 h-5 text-blue-600 border-gray-300 focus:ring-blue-500" />
+              <span>여성</span>
+            </label>
+            <label className="flex items-center space-x-2">
+              <input type="radio" name="gender" value="other" className="w-5 h-5 text-blue-600 border-gray-300 focus:ring-blue-500" />
+              <span>기타</span>
+            </label>
+          </div>
 
 
 
-    </div>
+
+          {/* 병합된 한 줄 - col-span-2 */}
+          <div className="border p-4 col-span-2 flex items-center space-x-2">
+            <input type="checkbox" className="w-5 h-5 text-blue-600 border-gray-300 rounded focus:ring-blue-500" />
+            <span className="text-[#9A9A9A]">위치 정보 제공에 동의합니다.</span>
+          </div>
+
+
+
+        </div>
+
+        <button className="rounded-[8px] w-[86px] h-[40px] bg-[#000000] text-white border mt-[30px] ">
+          저장
+        </button>
+
+
+
+      </div>
+    </div >
   );
 
 }
